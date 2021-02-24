f29f941a34fde6ae448180db1c9faab9576bd08c
Randolph Paul <randolphpaul@live.com>
2021-02-23 21:43:15 -0700

    corrected google button type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,7 +51,11 @@ export default class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign In </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               {' '}
               Sign In with Google{' '}
             </CustomButton>
